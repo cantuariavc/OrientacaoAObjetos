2221e464f8ebdade3a300fbe3de8624bb09b8342
Vinícius Cantuária <cantuariavc@gmail.com>
2017-06-27 16:52:46 -0300

    Add class DisciplinaNaoAtribuidaException

---
+++ src/controle/excecao/DisciplinaNaoAtribuidaException.java
@@ -0,0 +1,13 @@
+package controle.excecao;
+
+public class DisciplinaNaoAtribuidaException extends Exception {
+
+	public DisciplinaNaoAtribuidaException(String msg) {
+		super(msg);
+	}
+	
+	public DisciplinaNaoAtribuidaException(String msg, Throwable cause){
+        super(msg, cause);
+    }
+	
+}
