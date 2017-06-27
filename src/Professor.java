2ba486c0a956f9e410a8c010b381ba0ecd2cf4f6
Vinícius Cantuária <cantuariavc@gmail.com>
2017-06-27 14:23:37 -0300

    Add class Professor

---
+++ src/Professor.java
@@ -0,0 +1,40 @@
+
+public abstract class Professor {
+
+	private int matriculaSiape;
+	private int matriculaFUP;
+	private String formacao;
+	private float salario;
+	
+	
+	public int getMatriculaSiape() {
+		return matriculaSiape;
+	}
+	public void setMatriculaSiape(int matriculaSiape) {
+		this.matriculaSiape = matriculaSiape;
+	}
+	
+	public int getMatriculaFUP() {
+		return matriculaFUP;
+	}
+	public void setMatriculaFUP(int matriculaFUP) {
+		this.matriculaFUP = matriculaFUP;
+	}
+	
+	public String getFormacao() {
+		return formacao;
+	}
+	public void setFormacao(String formacao) {
+		this.formacao = formacao;
+	}
+	
+	public float getSalario() {
+		return salario;
+	}
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
+	
+	
+	
+}
