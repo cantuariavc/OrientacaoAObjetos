0a29a1e7868d1ee3eedb6aadf1f0f96e97eecde1
Vinícius Cantuária <cantuariavc@gmail.com>
2017-06-27 14:18:14 -0300

    Add class Graduacao

---
+++ src/Graduacao.java
@@ -0,0 +1,41 @@
+import java.util.Date;
+
+public class Graduacao extends Aluno {
+
+	private String formaIngresso;
+	private String curso;
+	private Date provavelFormatura;
+	
+	public Graduacao() {
+		
+	}
+	
+	public Graduacao(String formaIngresso, String curso, Date provavelFormatura) {
+		setFormaIngresso(formaIngresso);
+		setCurso(curso);
+		setProvavelFormatura(provavelFormatura);
+	}
+
+	
+	public String getFormaIngresso() {
+		return formaIngresso;
+	}
+	public void setFormaIngresso(String formaIngresso) {
+		this.formaIngresso = formaIngresso;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public Date getProvavelFormatura() {
+		return provavelFormatura;
+	}
+	public void setProvavelFormatura(Date provavelFormatura) {
+		this.provavelFormatura = provavelFormatura;
+	}
+	
+}
