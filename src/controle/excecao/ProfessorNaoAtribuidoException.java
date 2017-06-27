6fd97f0c289a02dee710327c52176fba8bd4e511
Vinícius Cantuária <cantuariavc@gmail.com>
2017-06-27 16:54:08 -0300

    Add class ProfessorNaoAtribuidoException

---
+++ src/controle/excecao/ProfessorNaoAtribuidoException.java
@@ -0,0 +1,13 @@
+package controle.excecao;
+
+public class ProfessorNaoAtribuidoException extends Exception {
+
+	public ProfessorNaoAtribuidoException(String msg) {
+		super(msg);
+	}
+	
+	public ProfessorNaoAtribuidoException(String msg, Throwable cause){
+        super(msg, cause);
+    }
+	
+}
