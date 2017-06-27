53c7efc0e7c670dea4e873fb96a56ae11d8b2b14
Vinícius Cantuária <cantuariavc@gmail.com>
2017-06-27 17:22:41 -0300

    Add class Executora

---
+++ src/controle/executora/Executora.java
@@ -0,0 +1,14 @@
+package controle.executora;
+import visao.*;
+
+public class Executora {
+	
+	static Principal visao;
+	
+	public static void main(String[] args) {
+
+		visao = new Principal();
+		
+	}
+
+}
