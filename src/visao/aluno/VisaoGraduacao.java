25a7be9d6a0d3840974b860f695ac5aa06da2929
Vinícius Cantuária <cantuariavc@gmail.com>
2017-07-02 14:30:26 -0300

    Add action cadastra

--- src/visao/aluno/VisaoGraduacao.java
+++ src/visao/aluno/VisaoGraduacao.java
@@ -3,11 +3,19 @@ package visao.aluno;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import controle.excecao.DisciplinaNaoAtribuidaException;
+import controle.excecao.InformacaoFaltanteException;
+import controle.excecao.ProfessorNaoAtribuidoException;
+import controle.executora.Executora;
+import dados.aluno.Graduacao;
 
 public class VisaoGraduacao extends JFrame implements ActionListener {
 
@@ -39,7 +47,7 @@ public class VisaoGraduacao extends JFrame implements ActionListener {
 		lMatricula = new JLabel("\t\t\t\t\t\t\t Matrícula:");
 		lNome = new JLabel("\t\t\t\t\t\t\t Nome:");
 		lSemestreIngresso = new JLabel("\t\t\t\t\t\t\t Semestre de Ingresso:");
-		lFormaIngresso = new JLabel("\t\t\t\t\t\t\t Professor:");
+		lFormaIngresso = new JLabel("\t\t\t\t\t\t\t Forma de Ingresso:");
 		lCurso = new JLabel("\t\t\t\t\t\t\t Curso:");
 		lProvavelFormatura = new JLabel("\t\t\t\t\t\t\t Provável Formatura:");
 		lVazio1 = new JLabel();
@@ -80,6 +88,7 @@ public class VisaoGraduacao extends JFrame implements ActionListener {
 		setVisible(true);
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == bLimpar) {
@@ -90,7 +99,41 @@ public class VisaoGraduacao extends JFrame implements ActionListener {
 			tfCurso.setText(null);
 			tfProvavelFormatura.setText(null);
 		} else if(e.getSource() == bCadastrar) {
+			try{
+				if (!tfMatricula.getText().isEmpty()) {
+					if (!tfNome.getText().trim().isEmpty()) {
+						if (!tfSemestreIngresso.getText().trim().isEmpty()) {
+							if (!tfFormaIngresso.getText().trim().isEmpty()) {
+								if (!tfCurso.getText().trim().isEmpty()) {
+									if (!tfProvavelFormatura.getText().trim().isEmpty()) {
+										Executora.aluno.cadastraGraduacao(new Graduacao(Integer.parseInt(tfMatricula.getText()), tfNome.getText(), tfSemestreIngresso.getText(), tfFormaIngresso.getText(), tfCurso.getText(), new Date(tfProvavelFormatura.getText())));
+									} else {
+										throw new InformacaoFaltanteException("Provavel Formatura não informada!");	
+									}
+								} else {
+									throw new InformacaoFaltanteException("Curso não informado!");
+								}
+							} else {
+								throw new InformacaoFaltanteException("Forma Ingresso não informado!");
+							}
+						} else {
+							throw new InformacaoFaltanteException("Semestre de Ingresso não informado!");
+						}
+					} else {
+						throw new InformacaoFaltanteException("Nome não informado!");
+					}
+				} else {
+					throw new InformacaoFaltanteException("Matrícula não informada!");
+				}
+			} catch (InformacaoFaltanteException e1) {
+				e1.printStackTrace();
+			} catch (NumberFormatException e2) {
+				JOptionPane.showMessageDialog(null, "Coloque somente números na matrícula!");
+			} catch (IllegalArgumentException e3) {
+				JOptionPane.showMessageDialog(null, "Coloque a provável formatura no modelo dia/mês/ano!");
+			}
 			
+						
 		} else if (e.getSource() == bVoltar) {
 			dispose();
 		}
