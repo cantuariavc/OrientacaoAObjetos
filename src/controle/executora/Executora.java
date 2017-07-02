86b4b5551a543614024eea8034c47556da8f1dca
Vinícius Cantuária <cantuariavc@gmail.com>
2017-07-02 19:55:36 -0300

    Add statics controles

--- src/controle/executora/Executora.java
+++ src/controle/executora/Executora.java
@@ -1,12 +1,26 @@
 package controle.executora;
+
+import controle.aluno.ControleAluno;
+import controle.disciplina.ControleDisciplina;
+import controle.professor.ControleProfessor;
+import controle.turma.ControleTurma;
 import visao.principal.*;
 
 public class Executora {
 	
 	static Principal visao;
+	public static ControleAluno aluno;
+	public static ControleProfessor professor;
+	public static ControleDisciplina disciplina;
+	public static ControleTurma turma;
 	
 	public static void main(String[] args) {
-
+		
+		aluno = new ControleAluno();
+		professor = new ControleProfessor();
+		disciplina = new ControleDisciplina();
+		turma = new ControleTurma();
+		
 		visao = new Principal();
 		
 	}
