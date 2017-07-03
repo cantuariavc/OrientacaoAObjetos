6b07ad6386f4573a94a255cee95561cf3786cf92
Vinícius Cantuária <cantuariavc@gmail.com>
2017-07-03 18:36:19 -0300

    Fix cadastrar

--- src/visao/disciplina/VisaoDisciplina.java
+++ src/visao/disciplina/VisaoDisciplina.java
@@ -3,11 +3,23 @@ package visao.disciplina;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import controle.executora.Executora;
+import dados.aluno.PosGraduacao;
+import dados.disciplina.Disciplina;
+import dados.excecao.InformacaoFaltanteException;
+import dados.excecao.OrientadorNaoAtribuidoException;
+import dados.professor.Professor;
 
 public class VisaoDisciplina extends JFrame implements ActionListener {
 
@@ -32,8 +44,8 @@ public class VisaoDisciplina extends JFrame implements ActionListener {
 		lCargaHoraria = new JLabel("\t\t\t\t\t\t\t Carga Horária:");
 		lVazio1 = new JLabel();
 		lVazio2 = new JLabel();
-		tfNome = new JTextField(15);
-		tfCargaHoraria = new JTextField(15);
+		tfNome = new JTextField("Ex.: Fulano de Tal", 15);
+		tfCargaHoraria = new JTextField("Ex.: 200", 15);
 		bLimpar = new JButton("Limpar");
 		bCadastrar = new JButton("Cadastrar");
 		bVoltar = new JButton("Voltar");
@@ -62,7 +74,22 @@ public class VisaoDisciplina extends JFrame implements ActionListener {
 			tfNome.setText(null);
 			tfCargaHoraria.setText(null);
 		} else if(e.getSource() == bCadastrar) {
-			
+			try {
+				if (!tfNome.getText().trim().isEmpty()) {
+					if (!tfCargaHoraria.getText().isEmpty()) {
+						Executora.disciplina.cadastraDisciplina(new Disciplina(tfNome.getName(), Integer.parseInt(tfCargaHoraria.getText())));
+						JOptionPane.showMessageDialog(null, "Disciplina cadastrado com sucesso!");
+					} else {
+						throw new InformacaoFaltanteException("Carga horária não informada!");
+					}
+				} else {
+					throw new InformacaoFaltanteException("Nome não informado!");
+				}
+			} catch (InformacaoFaltanteException e1) {
+				e1.printStackTrace();
+			} catch (NumberFormatException e2) {
+				JOptionPane.showMessageDialog(null, "Coloque somente números na carga horária!");
+			}
 		} else if (e.getSource() == bVoltar) {
 			dispose();
 		}
